c045872bd5715668411f1c6a0e359d82ad9e7371
moritalous <moritalous@gmail.com>
2023-05-14 11:49:28 +0000

    Initial commit

---
+++ test/tm-mp-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TmMpCdk from '../lib/tm-mp-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tm-mp-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TmMpCdk.TmMpCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
